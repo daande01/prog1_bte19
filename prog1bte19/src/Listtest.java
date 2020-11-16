a6315798b128999226472c322e8b875ed828f2b7
daande01 <daniel.evert.andersson@gmail.com>
2020-11-16 13:04:48 +0100

    rik.java

---
+++ prog1bte19/src/Listtest.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+
+public class Listtest {
+public static void main(String[] args) {
+
+
+	LinkedList<bil> a = new LinkedList<bil>();
+
+
+
+	a.add("hej");
+	a.add("då");
+	a.add(1, "ffff");
+
+
+	for (String variabel: a) {
+		System.out.println(variabel);
+
+	}
+
+
+
+
+
+}
+}
