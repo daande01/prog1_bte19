1f7e406426fe1bf215ded4ae58cb67d4b9b0d4f0
danand004 <danielevertandersson>
2020-11-30 14:41:23 +0100

    new file

--- prog1bte19/src/loopar/ovn89.java
+++ prog1bte19/src/loopar/ovn89.java
@@ -6,7 +6,15 @@ public class ovn89 {
 
 	public static void main(String[] args) {
 		int val;
+		int saldo=1000;
 		Scanner sc = new Scanner(System.in);
+		final int i=20 ;
+
+		Scanner sc1 = new Scanner(System.in);
+
+
+		double d = Math.PI;
+
 
 		do {
 			System.out.println("MENY \n 1.insättning \n 2.uttag \n 3.visa saldo\n 4.avsluta");
---
+++ prog1bte19/src/loopar/upp82.java
@@ -0,0 +1,25 @@
+package loopar;
+
+public class upp82 {
+
+	public static void main(String[] args) {
+
+		int [] a= {23,45,67,78,79};
+
+
+		//for (start,villkor,förändring)
+		for (int i = 4; i>=0; i--) {
+
+			System.out.println(a[i]);
+
+
+		}
+
+
+
+
+	}
+
+
+
+}
