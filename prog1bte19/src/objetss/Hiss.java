4d41b169ce8a4f4a66c77819b26f20ad735138ea
DANAND004 <danielevertandersson>
2021-01-21 12:03:03 +0100

    new file

---
+++ prog1bte19/src/objetss/Hiss.java
@@ -0,0 +1,66 @@
+package objetss;
+
+public class Hiss {
+
+	private int våning;
+	private int riktning;
+
+	public Hiss() {
+
+		våning = 0;
+		riktning = 0;
+
+		System.out.println("v:" + våning + " r:" + riktning);
+	}
+
+	public void moveTo() {
+
+		våning = 10;
+		System.out.println("v:" + våning + " r:" + riktning);
+	}
+
+	public void moveTo(int v)  {
+
+		if (v >= 0 && v <= 20) {
+
+			if (v > våning) {
+
+				for (int i = våning; i < v; i++) {
+					riktning=1;
+					våning++;
+					try {
+						Thread.sleep(2000);
+					} catch (InterruptedException e) {
+
+						e.printStackTrace();
+
+					}
+					System.out.println("v:" + våning + " r:" + riktning);
+
+				}
+				riktning=0;
+			}
+
+		}
+		System.out.println("du är framme v:" + våning + " r:" + riktning);
+	}
+
+	public int getVåning() {
+		return våning;
+	}
+
+	public int getRiktning() {
+		return riktning;
+	}
+
+	public static void main(String[] args)  {
+
+		Hiss h1 = new Hiss();
+		// System.out.println(h1.getVåning());
+		h1.moveTo(3);
+
+		new Hiss();
+
+	}
+
+}
