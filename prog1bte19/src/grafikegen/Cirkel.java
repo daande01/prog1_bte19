efe4be6b6cf34ce724804a6f4447434381443175
daande1 <danielevertandersson>
2021-02-04 09:43:36 +0100

    new file

--- prog1bte19/src/grafikegen/Cirkel.java
+++ prog1bte19/src/grafikegen/Cirkel.java
@@ -1,40 +1,85 @@
 package grafikegen;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
-public class Cirkel extends JComponent {
+public class Cirkel extends JComponent implements KeyListener{
 
 	private int x;
 	private int y;
+	private int xv=3;
+	private int yv=1;
+	private Color cc;
+	private int r=25;
+	private int wy=500;
+	private int wx= 500;
 
-	public Cirkel() {
 
-		x = 10;
-		y = 10;
-		Timer t = new Timer(100, e -> {
+
+	public Cirkel(Color c) {
+		cc=c;
+		this.setPreferredSize(new Dimension(wx,wy));
+		x = 0;
+		y = 0;
+		Timer t = new Timer(10, e -> {
 
 			update();
 			repaint();
 
+			wy=getSize().height;
+			wx=getSize().width;
 		});
 		t.start();
+
+
 	}
 	public void update() {
-		x++;
-		y++;
+
+		x+=xv;
+		y+=yv;
+
+
+		if (y>=(wy-(r*2))) {
+
+			yv*=-1;
+
+
+		}
+		if (x>=(wx-(r*2))) {
+
+			xv*=-1;
+
+		}
+		if (y<=0) {
+
+			yv*=-1;
+
+		}
+		if (x<=0) {
+
+			xv*=-1;
+
+		}
+
+
+
+
+
 	}
 	@Override
-	public void paintComponent(Graphics g) {
+	public void paint(Graphics g) {
 		super.paintComponents(g);
-		g.setColor(Color.BLUE);
+		g.setColor(cc);
 
-		g.fillOval(x, y, 100, 100);
+		g.fillOval(x, y, r*2, r*2);
 
 	}
 
@@ -42,9 +87,25 @@ public class Cirkel extends JComponent {
 
 		JFrame j = new JFrame();
 		j.setLayout(new FlowLayout());
-		j.setContentPane(new Cirkel());
+		j.setContentPane(new Cirkel(Color.CYAN));
 		j.setVisible(true);
-		j.setSize(500, 500);
+		j.pack();
+
+		j.setDefaultCloseOperation(j.EXIT_ON_CLOSE);
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
 
 	}
 
